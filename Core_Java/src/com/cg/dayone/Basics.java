21a2128c5af85493f2705b1bf083c3fc3b7590a3
malai <malai@DESKTOP-IOQG79T>
2023-10-19 11:05:24 +0530

    Commit

--- Core_Java/src/com/cg/dayone/Basics.java
+++ Core_Java/src/com/cg/dayone/Basics.java
@@ -5,6 +5,7 @@ public class Basics {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("The is the sum " +(12+8));
 
 	}
 
